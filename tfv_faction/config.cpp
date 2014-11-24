61ef7da77c7cc2b8e8094cd026b1bbce18390e41
leloberg <larserikloberg@gmail.com>
2014-11-24 09:13:24 +0100

    Changed faction to be reliant on ARMA vanilla only

--- tfv_faction/config.cpp
+++ tfv_faction/config.cpp
@@ -5,13 +5,7 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.0;
-		requiredAddons[] = {
-			"NOR_units",
-			"NOR_Core",	
-			"NOR_Backpacks",
-			"NOR_Equip",
-			"NOR_Law",
-			"NOR_Uniforms"};
+		requiredAddons[] = {};
 		author[] = {"Pedersen/Loberg"};
 		version = 0.1;
 		versionStr = "0.1";
@@ -25,8 +19,8 @@ class cfgFactionClasses
 		displayname="TF Valkyrie";
 		side=1;
 		priority=2;
-		flag="\tfv_faction\data\nor_faction_flag.paa";
-		icon="\tfv_faction\data\nor_faction_icon.paa";
+		flag="\tfv_faction\data\tfv_faction_flag.paa";
+		icon="\tfv_faction\data\tfv_faction_icon.paa";
 	};
 };
 
@@ -40,9 +34,18 @@ class CfgVehicleClasses
 
 class CfgVehicles
 {
-	class NOR_SL_Wood;
+	class B_medic_F;
+	class B_Soldier_SL_F;
+	class B_Soldier_TL_F;
+	class B_soldier_AR_F;
+	class B_soldier_AT_F;
+	class B_soldier_M_F;
+	class B_sniper_F;
+	class B_crew_F;
+	class B_spotter_F;
+	class B_soldier_exp_F;
 	
-		class TFV_SL_Wood: NOR_SL_Wood
+	class TFV_SL_Wood: B_Soldier_SL_F
 		{
 		side = 1;
 		faction = "TFV_Units";
