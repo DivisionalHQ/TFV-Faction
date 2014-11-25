d99857002c1ee9e44a2c0888ed5bb010241d5858
leloberg <larserikloberg@gmail.com>
2014-11-25 11:58:38 +0100

    created folder for equipment

---
+++ tfv_equip/config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches
+{
+	class tfv_custom_equip
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {};
+		author[] = {"Pedersen/Loberg"};
+		version = 0.1;
+		versionStr = "0.1";
+	};
+};
+
+class cfgWeapons
+{
+class V_PlateCarrier1_rgr;
+class TFV_Vest_Standard_Wood: V_PlateCarrier1_rgr
+	{
+		_generalMacro="V_PlateCarrier1_rgr";
+		scope=2;
+		picture="\tfv_equip\data\vest\ui\nor_carrier_rig_1_coyote_icon_ca.paa";
+		displayName="TFV Standard Vest";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\tfv_equip\data\vest\nor_carrier_standard_wood_co.paa"
+		};
+		author="Loberg";
+		class ItemInfo: VestItem
+		{
+			uniformModel="\A3\Characters_F\BLUFOR\equip_b_vest02";
+			containerClass="Supply140";
+			mass=60;
+			armor=20;
+			passThrough=0.5;
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+
+
+};
