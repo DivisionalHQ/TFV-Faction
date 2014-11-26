285847e0d12fc6877078ff281e6afebb63ada0b1
leloberg <larserikloberg@gmail.com>
2014-11-26 11:08:06 +0100

    cleaned down file to test functions

--- tfv_faction/config.cpp
+++ tfv_faction/config.cpp
@@ -47,48 +47,23 @@ class CfgVehicles
 	class B_crew_F;
 	class B_spotter_F;
 	class B_soldier_exp_F;
-	
-		// Officer Base - WIP, must change uniform
-		class TFV_Officer_Base_Wood: B_Soldier_SL_F
+	class TFV_Officer_Base_Wood: B_Soldier_SL_F
+	{
+		_generalMacro="B_Soldier_F";
+		scope=2;
+		scopeCurator=2;
+		side = 1;
+		faction="TFV_Units";
+		vehicleClass="TFV_Wood";
+		displayName="Test SL";
+		identityTypes[]= {"LanguageENG_F","Head_EURO"};
+		author="TFV";
+		nakedUniform="NOR_BasicBody";
+		uniformClass = "TFV_Uniform_Officer";
+		class EventHandlers
 		{
-			_generalMacro="B_Soldier_F";
-			scope=2;
-			scopeCurator=2;
-			side = 1;
-			faction="TFV_Units";
-			vehicleClass="TFV_Wood";
-			displayName="Test SL";
-			identityTypes[]=
-				{
-				"LanguageENG_F",
-				"Head_EURO"
-				};
-			author="TFV";
-			nakedUniform="NOR_BasicBody";
-			uniformClass = "TFV_Uniform_Officer";
-			/*hiddenSelections[] = {"camo1","camo2","insignia"};
-			
-			hiddenSelectionsTextures[]=
-				{
-					"\tfv_uniform\data\uniforms\nor_indep_uniform_wood_co.paa"
-				};*/
-				
-			class EventHandlers
-				{
-					init="[(_this select 0),""tfv_nor_flag_insignia""] call BIS_fnc_setUnitInsignia";
-				};
-		
+			init="[(_this select 0),""tfv_nor_flag_insignia""] call BIS_fnc_setUnitInsignia";
 		};
-		
-		
-	//Faction Classes - all units built upon base above, either Officer Base or Soldier Base. Pilot Base will be added. 
-	
-	class TFV_SL_Wood: TFV_Officer_Base_Wood
-		{
-		displayName = "Section Leader";
-		faction = "TFV_Units";
-		backpack=""; //not defined yet
-		};
-		
+	};
 };
 
